9918fbce571cb34b3ae1b06c002b423f0fe83f92
Shubh Pathak <shubhpathak07@gmail.com>
2020-08-06 23:37:03 +0530

    Removed About-Section

--- status/urls.py
+++ status/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     path('',views.HomeView.as_view(),name='home'),
-    path('about/',views.about,name='about'),
+    # path('about/',views.about,name='about'),
 ]
